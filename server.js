227cb9f0bae6ca8d1e23517ad8145177612f1020
philbjern <filipbiernat@gmail.com>
2024-08-26 16:12:21 +0200

    move auth and routes to separate modules

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict';
+const routes = require('./routes.js')
+const auth = require('./auth.js')
+
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
-
-const { ObjectID } = require('mongodb');
 
 const app = express();
 
@@ -31,56 +31,15 @@ app.use(passport.session());
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
-  app.route('/').get((req, res) => {
-    res.render('index', { title: 'Connected to Database', message: 'Please log in', showLogin: true });
-  });
+  routes(app, myDataBase);
 
-  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/profile');
-  })
-
-  function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect('/');
-  }
-
-  app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render('profile', { username: req.user.username });
-  })
-
-  app.route('/logout')
-    .get((req, res) => {
-      req.logout();
-      res.redirect('/');
-    });
-  
   app.use((req, res, next) => {
     req.status(404)
       .type('text')
       .send('Not Found');
   });
 
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-  
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-
-  passport.use(new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      console.log(`User ${username} attempted to log in.`);
-      if (err) return done(err);
-      if (!user) return done(null, false);
-      if (password !== user.password) return done(null, false);
-      return done(null, user);
-    });
-  }));
+  auth(app, myDataBase);
 
 }).catch(e => {
   app.route('/').get((req, res) => {
